87f03845e7a6c062e8643829554c28cc4daa59d4
KevinDev115 <>
2019-10-06 17:03:10 -0500

    Update NotasModel.php
    Se crean los siguientes metodos:
    
    ■ create = ejecuta el insert con los parametros pasados como parametros.
    ■ update= ejecuta el update con los parametros pasados como parametros.
    ■ delete = ejecuta el DELETE con el id pasado por parametro.
    ■ find = devuelve un registro segun el id.

--- app/models/NotasModel.php
+++ app/models/NotasModel.php
@@ -1,14 +1,78 @@
 <?php
 
+/**
+ * 
+ * Este modelo tiene todas las consultas SQL a la base de datos.
+ */
+
 class NotasModel extends DataBase
 {
     public function all()
     {
-        $sql = 'SELECT * FROM notas';
+        $sql = 'SELECT * FROM notas ORDER BY fecha_creacion DESC';
 
         try {
             $this->query($sql);
 			$res = $this->getAll();
+			return $res;
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+    }
+
+    public function create($data)
+    {
+        $sql = 'INSERT INTO notas (contenido, color) VALUES (?,?)';
+
+        try {
+            $this->query($sql);
+            $this->bind(1,$data['contenido']);
+            $this->bind(2,$data['color']);
+			$res = $this->execute();
+			return $res;
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+    }
+
+    public function update($data)
+    {
+        $sql = 'UPDATE notas SET contenido = ?, color = ? WHERE id = ?';
+
+        try {
+            $this->query($sql);
+            $this->bind(1,$data['contenido']);
+            $this->bind(2,$data['color']);
+            $this->bind(3,$data['id']);
+			$res = $this->execute();
+			return $res;
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+    }
+
+    public function delete($id)
+    {
+        $sql = 'DELETE FROM notas WHERE id = ?';
+
+        try {
+            $this->query($sql);
+            $this->bind(1,$id);
+			$res = $this->execute();
+			return $res;
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+    }
+
+    public function find($id)
+    {
+        $sql = 'SELECT * FROM notas WHERE id = ?';
+
+        try {
+            $this->query($sql);
+            $this->bind(1, $id);
+			$res = $this->getOne();
 			return $res;
 		} catch (PDOException $e) {
 			die($e->getMessage());
