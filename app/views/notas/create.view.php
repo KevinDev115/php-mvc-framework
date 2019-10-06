4f06078898ed13a0f2e057a10d98e7c283d753e2
KevinDev115 <>
2019-10-06 17:04:00 -0500

    Create create.view.php
    Vista para crear una nota nueva.

---
+++ app/views/notas/create.view.php
@@ -0,0 +1,19 @@
+<div class="container mt-5">
+    <h2>Crear una Nota</h2>
+    <form method="POST" action="<?=APP_URL?>/nota/store" class="border p-5 bg-white">
+        <div class="form-group row">
+            <label for="color" class="col-sm-2 col-form-label">Color</label>
+            <div class="col-sm-10">
+                <input type="color" class="form-control" id="color" name="color" value="<?=isset($_POST['color']) ? $_POST['color'] : null;?>">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="contenido" class="col-sm-2 col-form-label">Contenido</label>
+            <div class="col-sm-10">
+                <textarea class="form-control" name="contenido" id="contenido" ><?=isset($_POST['contenido']) ? $_POST['contenido'] : null;?></textarea>
+            </div>
+        </div>
+        <span class="text-danger"><?=!empty($data['error']) ? $data['error'] : $data['error']?></span>
+        <button type="submit" class="btn btn-success float-right">Crear</button>
+    </form>
+</div>
