671b9e91be75008d5aa3171fad37fa4af157275d
KevinDev115 <>
2019-10-06 17:01:18 -0500

    Update NotaController.php
    Se crean los siguientes metodos:
    
    NotaController
    ■  create = muestra la vista para crear una nueva nota
    ■  store= Se encarga de validar la informacion y guardarla.
    ■  edit = Trae la informacion de 1 registro por id y la muestra para editarla.
    ■ update = actualiza un registro segun su id
    ■ destroy = elimina un registro segun su id

--- app/controllers/NotaController.php
+++ app/controllers/NotaController.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * 
+ * 
+ * 
+ */
 class Nota extends Controller
 {
     protected $notaModel;
@@ -35,21 +39,101 @@ class Nota extends Controller
     /**
      * @author Kevin Galindo
      * 
+     * Muestra la vista para ingresar una nueva nota. 
+     */
+    public function create($errors = null)
+    {
+        #llamamos a la vista
+        require_once '../app/views/layouts/header.php';
+        $this->view('notas/create', [
+            'error' => $errors
+        ]);
+        require_once '../app/views/layouts/footer.php';
+    }
+
+    /**
+     * @author Kevin Galindo
+     * 
      */
     public function store()
     {
-        if (isset($_POST['contenido'])) {
-            
-        }else{
-            
+        $contenido = trim($_POST['contenido']);
+        $color = trim($_POST['color']);
+        $errors = '';
+
+        if (empty($contenido)) {
+            $errors = 'El campo "contenido" es obligatorio <br>';
         }
-        $data = [
-            'notas' => $res
-        ];
+
+        if (empty($errors)) {
+            $res = $this->notaModel->create($_POST);
+            if ($res) {
+                header('Location: '.APP_URL.'/nota');
+            }else{
+                $this->create('Error al crear. <br>');
+            }
+        }else{
+            $this->create($errors);
+        }
+    }
+
+    /**
+     * @author Kevin Galindo
+     * 
+     * Traer la informacion de un registro y mostrarla para editarla.
+     */
+    public function edit($id)
+    {
+        #traemos la informacion del registro
+        $nota = $this->notaModel->find($id);
 
         #llamamos a la vista
         require_once '../app/views/layouts/header.php';
-        $this->view('notas/index',$data);
+        $this->view('notas/edit', [
+            'nota' => $nota
+        ]);
         require_once '../app/views/layouts/footer.php';
+    }
+
+    /**
+     * @author Kevin Galindo
+     * @param integer $id
+     * Actualizar un registro segun su id
+     */
+    public function update()
+    {
+        $contenido = trim($_POST['contenido']);
+        $color = trim($_POST['color']);
+        $errors = '';
+
+        if (empty($contenido)) {
+            $errors = 'El campo "contenido" es obligatorio <br>';
+        }
+
+        if (empty($errors)) {
+            $res = $this->notaModel->update($_POST);
+            if ($res) {
+                header('Location: '.APP_URL.'/nota');
+            }else{
+                die('Error al crear. <br>');
+            }
+        }else{
+            die($errors);
+        }
+    }
+
+    /**
+     * @author Kevin Galindo
+     * 
+     * Elimina un registro segun su id.
+     */
+    public function destroy($id)
+    {
+        $res = $this->notaModel->delete($id);
+        if ($res) {
+            header('Location: '.APP_URL.'/nota');
+        }else{
+            die('Error al crear. <br>');
+        }
     }
 }
