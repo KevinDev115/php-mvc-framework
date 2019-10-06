d797c783025949d70ae3574b7cadae15da9743ae
ArtoriasDeveloper <40973272+ArtoriasDeveloper@users.noreply.github.com>
2019-10-06 15:34:49 -0500

    Vistas
    Se crea la vista Home/index.view.php.
    Se modifica la vista nota/index.view.php

---
+++ app/views/home/index.view.php
@@ -0,0 +1,15 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Framework PHP-MVC</h1>
+    <b>By:</b> <span>Kevin Galindo</span>
+    <p class="lead">
+        Este framework trabaja la estructura MVC (Modelo, Vista, Controlador) facilitando la creacion en cada una de ellas.
+    </p>
+    <p class="lead">Ejemplos:</p>
+    <ul>
+        <li>
+            <a href="<?=APP_URL?>/nota/">Notas</a>
+        </li>
+    </ul>
+  </div>
+</div>
--- app/views/notas/index.view.php
+++ app/views/notas/index.view.php
@@ -1,14 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=APP_NAME?> - <?=APP_VERSION?></title>
-</head>
-<body>
-    <h2>Notas: </h2>
 
-    <?php var_dump_pro($data); ?>
-</body>
-</html>
+<h2>Notas: </h2>
+
+<div class="container">
+    <div class="row">
+        <h2>Notas</h2>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 table-responsive">
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Texto</th>
+                        <th>Fecha Creacion</th>
+                        <th>
+                            <span>Opciones</span>
+                        </th>
+                        <th class="text-center">
+                            <button class="btn btn-success">ADD</button>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>                    
+                    <?php foreach($data['notas'] as $key => $value ): ?>
+                    <tr style="background: <?=$value->color?>;">
+                        <td><?=$value->id?></td>
+                        <td><?=$value->contenido?></td>
+                        <td><?=$value->fecha_creacion?></td>
+                        <td class="text-center" colspan="2">
+                            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                <button class="btn btn-secondary">Editar</button>
+                                <button class="btn btn-danger">Eliminar</button>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+
+var_dump($data);
+?>
+
